bbfbb28dae0fcf776984cb7e22a44edea3c7e3a6
fariel huang <farielclaire@gmail.com>
2020-06-28 17:15:27 +0800

    push

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -22,12 +22,12 @@ under the License.
     class="home"
     aria-labelledby="main-title"
   >
-    <header class="hero">
-      <img
+    <header class="hero" :style="`background: url(${$withBase(data.heroImage)}) no-repeat top center`">
+      <!-- <img
         v-if="data.heroImage"
         :src="$withBase(data.heroImage)"
         :alt="data.heroAlt || 'hero'"
-      >
+      > -->
       <h1
         v-if="data.heroText !== null"
         id="main-title"
@@ -52,61 +52,97 @@ under the License.
       </p>
     </header>
 
-    <div class="structure wrapper">
+    <div class="structure wrapper" style="margin-top: -300px;">
       <h1>
-        Apache Doris
+        {{ data.structure.text }}
       </h1>
-      <img
-        v-if="data.structureImage"
-        :src="$withBase(data.structureImage)"
-        alt="structure"
-        style="width: 100%;"
-      >
-    </div>
-    <div style="width: 100%; background-color: #f7f7f9;">
-      <div 
-        v-if="data.features && data.features.length"
-        class="wrapper"
-      >
-        <h1 v-if="data.featuresText">
-          {{ data.featuresText }}
-        </h1>
-        <div class="features">
-          <div
-            v-for="(feature, index) in data.features"
-            :key="index"
-            class="feature"
+      <div class="divider" />
+      <div class="sub-text">
+        {{ data.structure.subText }}
+      </div>
+      <div class="intro">
+        <div class="text">
+          <div class="title">
+            {{ data.structure.title }}
+          </div>
+          <div v-for="item in data.structure.descriptions" :key="item" class="description">
+            {{ item }}
+          </div>
+          <NavLink
+            class="action-button"
+            :item="moreActionLink"
+          />
+        </div>
+        <div class="image">
+          <img
+            v-if="data.structure.image"
+            :src="$withBase(data.structure.image)"
+            alt="structure"
+            style="width: 100%;"
           >
-            <h2>
-              {{ feature.title }}
-            </h2>
+        </div>
+      </div>
+    </div>
+    <div 
+      v-if="data.features"
+      class="wrapper features"
+    >
+      <h1>
+        {{ data.features.text }}
+      </h1>
+      <div class="divider" />
+      <div class="sub-text">
+        {{ data.features.subText }}
+      </div>
+      <div class="list">
+        <div
+          v-for="(feature, index) in data.features.list"
+          :key="index"
+          class="feature"
+        >
+          <div class="icon-card">
+            <img
+              v-if="feature.icon"
+              :src="$withBase(feature.icon)"
+              alt="feature"
+            >
+          </div>
+          <div class="label">
+            {{ feature.title }}
           </div>
         </div>
       </div>
     </div>
 
-    <div class="wrapper">
-      <h1 v-if="data.casesText">
-        {{ data.casesText }}
+    <div
+      v-if="data.cases"
+      class="wrapper cases">
+      <h1>
+        {{ data.cases.text }}
       </h1>
-      <div class="cases">
+      <div class="divider" />
+      <div class="sub-text">
+        {{ data.cases.subText }}
+      </div>
+      <div class="list">
         <div
-          v-for="(cese, index) in data.cases"
+          v-for="(cese, index) in data.cases.list"
           :key="index"
           class="case"
         >
           <img
-            v-if="cese.logo"
+            v-if="$withBase(cese.logo)"
             :src="cese.logo"
-            :alt="'image'"
-            style="height: 34px;"
+            :alt="cese.alt"
           >
         </div>
       </div>
     </div>
 
     <footer class="footer">
-      <CustomFooter />
+      <div class="pure_top">
+        <CustomFooter style="z-index: 1; position: relative; padding: 3rem 0rem" />
+      </div>
     </footer>
   </main>
 </template>
@@ -126,6 +162,12 @@ export default {
         link: this.data.actionLink,
         text: this.data.actionText
       }
+    },
+    moreActionLink () {
+      return {
+        link: this.data.structure.actionLink,
+        text: this.data.structure.actionText
+      }
     }
   }
 }
@@ -133,40 +175,43 @@ export default {
 
 <style lang="stylus">
 .home
-  padding $navbarHeight 0 0
+  overflow-x hidden
+  padding 0 0
   width 100%
   margin 0px auto
   display block
   .hero
     background-color rgb(247, 247, 249)
     color: rgb(76, 110, 165)
-    padding: 4em 2em 2em 2em
+    padding: 6em 2em 2em 2em
     text-align center
+    height 1100px
     img
       max-width: 100%
       max-height 200px
       display block
       margin 0 auto 1.5rem
     h1
-      font-size 3rem
+      font-size 2.8rem
+      color #333333
     h1, .description, .action
       margin 1.8rem auto
     .description
       font-size 1.6rem
       line-height 1.3
-      color lighten(rgb(76, 110, 165), 40%)
+      color #999999
     .action-button
       display inline-block
       font-size 1.2rem
       color #fff
-      background-color rgb(76, 110, 165)
-      padding 0.8rem 1.6rem
-      border-radius 4px
+      box-shadow 0 8px 12px 0 rgba(81,105,172,0.27)
+      background-color #5169AC
+      padding 0.4rem 1.6rem
+      border-radius 10px
       transition background-color .1s ease
       box-sizing border-box
-      border-bottom 1px solid darken(rgb(76, 110, 165), 10%)
       &:hover
-        background-color lighten(rgb(76, 110, 165), 10%)
+        background-color lighten(#5169AC, 10%)
   .wrapper
     box-sizing: border-box
     max-width: 1280px
@@ -174,91 +219,183 @@ export default {
     margin-left: auto;
     margin-right: auto;
     width: 100%;
+    h1
+      font-size 2rem
+      text-align center
+      color #5169AC
+    .sub-text
+      font-size 2rem
+      text-align center
+      color #CCCCCC
+      padding: 0.7rem 0rem
+    .divider 
+      height 2px
+      width 580px
+      margin 0 auto
+      opacity 0.05
+      background-image radial-gradient(50% 100%, #000000 50%, #D8D8D8 100%)
+  .structure
+    .intro
+      padding-top 2rem
+      display flex
+      flex-direction row
+      align-items flex-start
+      justify-content center
+      .text
+        padding 3rem 0rem 0rem
+        flex-basis 35%
+        .title
+          color #333333
+          line-height 4rem
+          font-size 1.6rem
+          font-weight bold
+        .description
+          padding 1.3rem 4rem 0rem 0rem
+          font-size 1.2rem
+          line-height 1.6rem
+          color #999999
+        .action-button
+          margin-top 2rem
+          display inline-block
+          font-size 1.2rem
+          color #fff
+          box-shadow 0 8px 12px 0 rgba(81,105,172,0.27)
+          background-color #5169AC
+          padding 0.4rem 1.6rem
+          border-radius 10px
+          transition background-color .1s ease
+          box-sizing border-box
+          &:hover
+            background-color lighten(#5169AC, 10%)
+      .image
+        background #FFFFFF
+        box-shadow 0 0 20px 0 rgba(81,105,172,0.10)
+        border-radius 16px
+        padding 1rem
+        flex-basis 65%
+        &:hover
+          box-shadow 0 0 20px 0 rgba(81,105,172,0.30)
   .features
-    padding 0.5rem 0
-    margin-top 2.5rem
-    display flex
-    flex-wrap wrap
-    align-items flex-end
-    align-content stretch
-    justify-content space-between
-  .feature
-    flex-grow 1
-    flex-basis 45%
-    max-width 45%
-    padding: 1rem
-    border-bottom 1px solid $borderColor
-    h2
-      font-size 1.0rem
-      font-weight 500
-      border-bottom none
-      padding-bottom 0
-      color lighten($textColor, 10%)
-      &:before
-        content "";
-        width 0.4rem;
-        height 0.4rem;
-        margin-right 0.5rem;
-        margin-top 0.5rem;
-        display block;
-        float left;
-        background-color $textColor;
-    p
-      color lighten($textColor, 25%)
+    .list
+      padding 0.5rem 0
+      margin-top 2.5rem
+      display flex
+      flex-wrap wrap
+      align-items flex-end
+      align-content stretch
+      justify-content space-between
+      .feature
+        flex-grow 1
+        flex-basis 18%
+        max-width 18%
+        padding: 1rem
+        .icon-card
+          width 8rem
+          height 8rem
+          margin 0 auto
+          background #FFFFFF
+          box-shadow 0 0 20px 0 rgba(81,105,172,0.10)
+          border-radius 16px
+          &:hover
+            box-shadow 0 0 20px 0 rgba(81,105,172,0.30)
+          img
+            height 3.6rem
+            padding 2.2rem
+        .label
+          font-size 1.2rem
+          font-weight 500
+          border-bottom none
+          color #333333
+          height 4rem
+          padding-top 1rem
+          margin 0 auto
+          text-align center
   .cases
-    padding 0.5rem 0
-    margin-top 2.5rem
-    display flex
-    flex-wrap wrap
-    align-items flex-end
-    align-content stretch
-    justify-content flex-start
-  .case
-    flex-grow 1
-    flex-basis 20%
-    max-width 20%
-    padding: 1rem 0rem
-    text-align center
+    .list
+      padding 0.5rem 0
+      margin-top 2.5rem
+      display flex
+      flex-wrap wrap
+      align-items flex-end
+      align-content stretch
+      justify-content space-between
+      .case
+        flex-grow 1
+        flex-basis 16%
+        max-width 16%
+        padding: 1rem 0rem
+        text-align center
+        img 
+          width 80%
+          margin 0 auto
   .footer
-    background-color #f7f7f9
-    border-top 1px solid $borderColor
     text-align center
     color lighten($textColor, 25%)
+    padding 3rem 0rem 0rem
+    .pure_top 
+      width 100%
+      height 27rem
+      position relative
+      z-index 0
+      overflow hidden
+      &:after
+        content ''
+        width 120%
+        height 27rem
+        position absolute
+        left -10%
+        top 0
+        border-radius 100% 100% 0 0
+        background #F7F7F9
 @media (max-width: $MQMobile)
   .home
-    .features
-      flex-direction column
-      align-items flex-start
-    .feature
-      max-width 100%
-      padding 0 2.5rem
-    .case
-      flex-basis 45%
-      max-width 45%
-@media (max-width: $MQMobileNarrow)
-  .home
-    padding-left 1.5rem
-    padding-right 1.5rem
     .hero
-      img
-        max-height 210px
-        margin 2rem auto 1.2rem
       h1
         font-size 2rem
-      h1, .description, .action
-        margin 1.2rem auto
+        color #333333
       .description
         font-size 1.2rem
-      .action-button
-        font-size 1rem
-        padding 0.6rem 1.2rem
     .wrapper
       h1
-        font-size: 2rem
-    .feature
-      h2
-        font-size 1.25rem
-    .case
-      flex-basis 45%
-      max-width 45%
+        font-size 1.6rem
+      .sub-text
+        font-size 1.6rem
+      .divider 
+        width 380px
+    .structure
+      .intro
+        flex-direction column
+        align-items center
+        justify-content flex-start
+        .text
+          order 1
+          flex-basis 100%
+          text-align center
+          .description
+            padding 1.3rem 0rem 0rem 0rem
+        .image
+          order 0
+          flex-basis 100%
+    .features
+      .list
+        .feature 
+          flex-basis 40%
+          max-width 40%
+    .cases
+      .list
+        .case
+          flex-basis 30%
+          max-width 30%
+    .footer
+      .pure_top 
+        height 30rem
+        &:after
+          content ''
+          width 200%
+          height 30rem
+          position absolute
+          left -50%
+          top 0
+          border-radius 50% 50% 0 0
+          background #F7F7F9
 </style>
--- docs/zh-CN/README.md
+++ docs/zh-CN/README.md
@@ -1,51 +1,72 @@
 ---
 home: true
-heroImage: /images/doris-logo.png
+heroImage: /images/home/hero_banner.png
 heroText: 欢迎使用 Apache Doris
 tagline: 支持对海量大数据进行快速分析的MPP数据库
-structureImage: /images/structure-fresh.png
-featuresText: Apache Doris 核心特性
+structure: 
+  text: Apache Doris数据交互
+  subText: Our Data Interaction
+  title: Apache Doris数据交互
+  descriptions: 
+    - 导入（Load）功能就是将用户的原始数据导入到 Doris 中。导入成功后，用户即可通过 Mysql 客户端查询数据。
+    - Doris 支持多种导入方式。建议先完整阅读本文档，再根据所选择的导入方式，查看各自导入方式的详细文档。
+  image: /images/structure-fresh.png
+  actionText: 了解更多
+  actionLink: /zh-CN/getting-started/basic-usage
 features:
-- title: 现代化MPP架构
-- title: 秒级查询返回延时
-- title: 支持标准SQL语言，兼容MySQL协议
-- title: 向量化执行器
-- title: 高效的聚合表技术
-- title: 新型预聚合技术Rollup
-- title: 高性能、高可用、高可靠
-- title: 极简运维，弹性伸缩
-casesText: Apache Doris 用户
+  text: Apache Doris 核心特性
+  subText: Our Core Features
+  list: 
+    - title: 现代化MPP架构
+      icon: /images/home/struct.png
+    - title: 秒级查询返回延时
+      icon: /images/home/clock.png
+    - title: 支持标准SQL语言，兼容MySQL协议
+      icon: /images/home/sql.png
+    - title: 向量化执行器
+      icon: /images/home/program.png
+    - title: 高效的聚合表技术
+      icon: /images/home/aggr.png
+    - title: 新型预聚合技术Rollup
+      icon: /images/home/rollup.png
+    - title: 高性能、高可用、高可靠
+      icon: /images/home/cpu.png
+    - title: 极简运维，弹性伸缩
+      icon: /images/home/dev.png
 cases:
-- logo: https://s0.meituan.net/bs/fe-web-meituan/fa5f0f0/img/logo.png
-  alt: 美团
-- logo: https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3452306450,1932457124&fm=26&gp=0.jpg
-  alt: 小米
-- logo: https://img10.360buyimg.com/img/jfs/t1/117726/20/7368/88789/5ec3777cE70ffaf64/47d8f5d0310958bd.gif
-  alt: 京东
-- logo: https://www-file.huawei.com/-/media/corporate/images/home/logo/huawei_logo.png
-  alt: 华为
-- logo: https://www.baidu.com/img/flexible/logo/pc/result@2.png
-  alt: 百度
-- logo: https://img.t.sinajs.cn/t6/style/images/global_nav/WB_logo-x2.png?id=1404211047727
-  alt: 新浪微博
-- logo: https://plat-h5.cdnjtzy.com/www/img/ca99311.png@f_webp
-  alt: 作业帮
-- logo: https://s.vipkidstatic.com/fe-static/parent/panda/web/plugs/semHeader/img/vipkidlogo_d4b868b3.png
-  alt: Vipkid
-- logo: https://p3.ssl.qhimg.com/t011e94f0b9ed8e66b0.png
-  alt: 360
-- logo: https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592298916686&di=11fdf737dacb3da8de9a0fe11972198c&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180917%2F5d4d8327b12c47e4b3f42e7b0e5a4373.jpeg
-  alt: Shopee
-- logo: https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1704289876,3870130483&fm=26&gp=0.jpg
-  alt: 探探
-- logo: https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592304349676&di=57f7f991b20e527a3a0ef4c277043775&imgtype=0&src=http%3A%2F%2Fpic.ossfiles.cn%3A9186%2Fgroup2%2FM00%2F0E%2F6B%2FrBgICV0dnIiASALXAAAtNU0p8U8473.jpg
-  alt: 快手
-- logo: https://statics.itc.cn/asm/static/images/logo-large.png
-  alt: 搜狐
-- logo: https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3333062732,1639132429&fm=26&gp=0.jpg
-  alt: 一点资讯
-- logo: https://www.100.me/themes/100me/images/logo.png
-  alt: 叮咚买菜
+  text: Apache Doris 用户
+  subText: Our Users
+  list:
+    - logo: /images/home/logo-meituan.png
+      alt: 美团
+    - logo: /images/home/logo-xiaomi.png
+      alt: 小米
+    - logo: /images/home/logo-jd.png
+      alt: 京东
+    - logo: /images/home/logo-huawei.png
+      alt: 华为
+    - logo: /images/home/logo-baidu.png
+      alt: 百度
+    - logo: /images/home/logo-weibo.png
+      alt: 新浪微博
+    - logo: /images/home/logo-zuoyebang.png
+      alt: 作业帮
+    - logo: /images/home/logo-vipkid.png
+      alt: Vipkid
+    - logo: /images/home/logo-360.png
+      alt: 360
+    - logo: /images/home/logo-shopee.png
+      alt: Shopee
+    - logo: /images/home/logo-tantan.png
+      alt: 探探
+    - logo: /images/home/logo-kuaishou.png
+      alt: 快手
+    - logo: /images/home/logo-sohu.png
+      alt: 搜狐
+    - logo: /images/home/logo-yidian.png
+      alt: 一点资讯
+    - logo: /images/home/logo-dingdong.png
+      alt: 叮咚买菜
 actionText: 快速上手 →
 actionLink: /zh-CN/installing/compilation
 ---
