fa1ae6674c20c07e24b0609eaf5bb7ac8421f825
fariel huang <farielclaire@gmail.com>
2020-06-30 20:00:46 +0800

    push

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -23,64 +23,59 @@ under the License.
     aria-labelledby="main-title"
   >
     <header class="hero">
-      <!-- <img
-        v-if="data.heroImage"
-        :src="$withBase(data.heroImage)"
-        :alt="data.heroAlt || 'hero'"
-      > -->
-      <div class="header-wrapper" style="position: relative;">
-        <div id="scene1" style="height: 100%; position: absolute;">
-          <img :src="$withBase(data.heroBgImage)"
-              data-hover-only="true"
-              data-depth="0.2"
-              data-relative-input="true" style="height: 100%;">
-        </div>
-        <div class="intro" style="z-index: 1;">
-          <div class="text">
-            <div
-              v-for="item in data.heroText"
-              :key="item"
-              class="title"
-            >
-              {{ item || $title || 'Hello' }}
-            </div>
-            <div
-              v-if="data.tagline !== null"
-              class="description"
-            >
-              {{ data.tagline || $description || 'Welcome to your VuePress site' }}
-            </div>
-            <p
-              v-if="data.actionText && data.actionLink"
-              class="action"
-            >
-              <NavLink
-                class="action-button"
-                :item="actionLink"
-              />
-            </p>
-          </div>
-          <div 
-            class="image" id="scene"
+      <div id="scene-bg">
+        <img 
+          :src="$withBase(data.heroBgImage)"
+          data-hover-only="true"
+          data-depth="-0.2"
+          data-relative-input="true"
+        >
+      </div>
+      <div class="intro">
+        <div class="text">
+          <div
+            v-for="item in data.heroText"
+            :key="item"
+            class="title"
           >
-            <img
-              data-hover-only="true"
-              data-depth="-0.3"
-              data-relative-input="true"
-              :src="$withBase(data.heroImage)"
-            >
+            {{ item || $title }}
           </div>
+          <div
+            v-if="data.tagline !== null"
+            class="description"
+          >
+            {{ data.tagline || $description }}
+          </div>
+          <p
+            v-if="data.actionText && data.actionLink"
+            class="action"
+          >
+            <NavLink
+              class="action-button"
+              :item="actionLink"
+            />
+          </p>
+        </div>
+        <div
+          id="scene"
+          class="image"
+        >
+          <img
+            :src="$withBase(data.heroImage)"
+            data-hover-only="true"
+            data-depth="0.3"
+            data-relative-input="true"
+          >
         </div>
       </div>
     </header>
 
-    <div class="structure wrapper" style="margin-top: 0px;">
+    <div class="structure wrapper">
       <div class="image">
         <img
           v-if="data.structure.image"
           :src="$withBase(data.structure.image)"
           alt="structure"
-          style="width: 100%;"
         >
       </div>
       <div class="text">
@@ -90,7 +85,11 @@ under the License.
         <div class="sub-title">
           {{ data.structure.subTitle }}
         </div>
-        <div v-for="item in data.structure.descriptions" :key="item" class="description">
+        <div 
+          v-for="item in data.structure.descriptions"
+          :key="item"
+          class="description"
+        >
           {{ item }}
         </div>
         <NavLink
@@ -133,7 +132,8 @@ under the License.
 
     <div
       v-if="data.cases"
-      class="wrapper cases">
+      class="wrapper cases"
+    >
       <div class="title">
         {{ data.cases.title }}
       </div>
@@ -165,26 +165,24 @@ under the License.
 import NavLink from '@parent-theme/components/NavLink.vue'
 import CustomFooter from '@theme/components/Footer.vue'
 import Parallax from 'parallax-js'
+
 export default {
   name: 'Home',
   components: { NavLink, CustomFooter },
   mounted() {
-    var scene = document.getElementById('scene');
-    var parallaxInstance = new Parallax(scene);
-    var scene1 = document.getElementById('scene1');
-    var parallaxInstance = new Parallax(scene1);
+    ['scene', 'scene-bg'].forEach(id => new Parallax(document.getElementById(id)))
   },
   computed: {
-    data () {
+    data() {
       return this.$page.frontmatter
     },
-    actionLink () {
+    actionLink() {
       return {
         link: this.data.actionLink,
         text: this.data.actionText
       }
     },
-    moreActionLink () {
+    moreActionLink() {
       return {
         link: this.data.structure.actionLink,
         text: this.data.structure.actionText
@@ -199,66 +197,64 @@ export default {
   overflow-x hidden
   padding 0 0
   width 100%
-  margin 0px auto
+  margin 0 auto
   display block
   font-family PingFangSC-Regular
   .hero
     background-color #5A77F3
-    color: rgb(76, 110, 165)
-    padding: 0em
-    text-align left
+    width 100%
     height 760px
-    .header-wrapper
+    display flex
+    flex-direction row
+    justify-content center
+    position relative
+    #scene-bg
+      height 100%
+      position absolute
+      img
+        height 100%
+    .intro
       display flex
       flex-direction row
-      justify-content center
-      box-sizing: border-box
-      margin-left auto
-      margin-right auto
-      width: 100%
-      height 100%
-      padding: 0rem 4rem
-      .intro
-        display flex
-        flex-direction row
-        align-items center
-        justify-content space-between
-        width 1280px
-        .text
-          .title
-            font-family PingFangSC-Semibold
-            font-size 3.1rem
-            color white
-            letter-spacing 0
-          .description
-            font-family PingFangSC-Light
-            padding 1.5rem 0rem 3rem
-            opacity 0.85
-            font-size 1.3rem
-            color #FFFFFF
-            letter-spacing 0.33px
-          .action-button
-            display inline-block
-            font-size 1.2rem
-            color white
-            border: 1px solid #FFFFFF;
-            padding 0.4rem 2.6rem
-            transition background-color .1s ease
-            &:hover
-              color #5A77F3
-              background-color white
-        .image
-          position relative
-          flex-basis 50%
-          img
-            width 530px
+      align-items center
+      justify-content space-between
+      width 1280px
+      z-index 1
+      .text
+        .title
+          font-family PingFangSC-Semibold
+          font-size 3.1rem
+          color white
+          letter-spacing 0
+        .description
+          font-size 1.3rem
+          font-family PingFangSC-Light
+          padding 1.5rem 0rem 3rem
+          opacity 0.85
+          color #FFFFFF
+          letter-spacing 0.33px
+        .action-button
+          display inline-block
+          font-size 1.2rem
+          color white
+          border 1px solid #FFFFFF
+          padding 0.4rem 2.6rem
+          transition background-color .1s ease
+          &:hover
+            color #5A77F3
+            background-color white
+      .image
+        position relative
+        flex-basis 50%
+        img
+          width 530px
   .wrapper
-    box-sizing: border-box
-    max-width: 1280px
-    padding: 2em 2em 2em 2em
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
+    box-sizing border-box
+    max-width 1280px
+    padding 2em 2em 2em 2em
+    margin-left auto
+    margin-right auto
+    width 100%
     .title
       font-size 1.9rem
       text-align center
@@ -270,12 +266,6 @@ export default {
       font-size 1.3rem
       color #999999
       padding-top 1rem
-    .divider 
-      height 2px
-      width 580px
-      margin 0 auto
-      opacity 0.05
-      background-image radial-gradient(50% 100%, #000000 50%, #D8D8D8 100%)
   .structure
     padding 8rem 0rem 5rem
     display flex
@@ -307,7 +297,7 @@ export default {
         display inline-block
         font-size 1rem
         color #5169AC
-        border: 1px solid #5169AC
+        border 1px solid #5169AC
         padding 0.4rem 2.8rem
         transition background-color .1s ease
         box-sizing border-box
@@ -316,6 +306,8 @@ export default {
           background-color #5169AC
     .image
       flex-basis 68%
+      img
+        width 100%
   .features
     padding 5rem 0rem
     background-color #F9FCFE
@@ -331,7 +323,7 @@ export default {
         flex-grow 1
         flex-basis 18%
         max-width 18%
-        padding: 1rem
+        padding 1rem
         .icon-card
           width 8rem
           height 8rem
@@ -345,8 +337,7 @@ export default {
           height 4rem
           margin 0 auto
           text-align center
-          font-family: PingFangSC-Regular;
-          color: #595959;
+          color #595959
   .cases
     padding 5rem 0rem
     .list
@@ -373,39 +364,31 @@ export default {
   .home
     .hero
       height 960px
-      .header-wrapper
-        .intro
-          display flex
-          flex-direction column
-          align-items center
-          justify-content flex-start
-          .text
-            padding 7rem 0rem 0rem
-            text-align center
-            .title
-              font-size 2rem
-            .description
-              width 90%
-              margin 0 auto
-              font-size 1.4rem
-              color white
-              opacity 0.6
-          .image
-            position relative
-            flex-basis 100%
-            width 100%
-      h1
-        font-size 2rem
-        color #333333
-      .description
-        font-size 1.2rem
+      .intro
+        display flex
+        flex-direction column
+        align-items center
+        justify-content flex-start
+        .text
+          padding 7rem 0rem 0rem
+          text-align center
+          .title
+            font-size 2rem
+          .description
+            width 90%
+            margin 0 auto
+            font-size 1.4rem
+            color white
+            opacity 0.6
+        .image
+          position relative
+          flex-basis 100%
+          width 100%
     .wrapper
       title
         font-size 1.6rem
       .sub-title
         font-size 1.6rem
-      .divider 
-        width 380px
     .structure
       flex-direction column
       align-items center
